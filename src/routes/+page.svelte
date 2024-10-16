f2e8d3be0170bdaff1d9e06e54975f5c33e5b82b
DamianR04 <damian.romeijn@hva.nl>
2024-10-16 17:06:44 +0200

    test4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+
+<!-- Navbar, which is on the bottom of the page -->
+
+
+
+<!-- The homepage, this has the following parts:
+ I+Web text
+ Heart SVG
+ Button to go to the next part of the site
+  -->
+
 <section class="homepage" id="home">
   <h1>I</h1>
     <svg height="120px" width="120px" class="heart-svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M474.655,74.503C449.169,45.72,413.943,29.87,375.467,29.87c-30.225,0-58.5,12.299-81.767,35.566 c-15.522,15.523-28.33,35.26-37.699,57.931c-9.371-22.671-22.177-42.407-37.699-57.931c-23.267-23.267-51.542-35.566-81.767-35.566 c-38.477,0-73.702,15.851-99.188,44.634C13.612,101.305,0,137.911,0,174.936c0,44.458,13.452,88.335,39.981,130.418 c21.009,33.324,50.227,65.585,86.845,95.889c62.046,51.348,123.114,78.995,125.683,80.146c2.203,0.988,4.779,0.988,6.981,0 c2.57-1.151,63.637-28.798,125.683-80.146c36.618-30.304,65.836-62.565,86.845-95.889C498.548,263.271,512,219.394,512,174.936 C512,137.911,498.388,101.305,474.655,74.503z"></path> <path d="M160.959,401.243c-36.618-30.304-65.836-62.565-86.845-95.889 c-26.529-42.083-39.981-85.961-39.981-130.418c0-37.025,13.612-73.631,37.345-100.433c21.44-24.213,49.775-39.271,81.138-43.443 c-5.286-0.786-10.653-1.189-16.082-1.189c-38.477,0-73.702,15.851-99.188,44.634C13.612,101.305,0,137.911,0,174.936 c0,44.458,13.452,88.335,39.981,130.418c21.009,33.324,50.227,65.585,86.845,95.889c62.046,51.348,123.114,78.995,125.683,80.146 c2.203,0.988,4.779,0.988,6.981,0c0.689-0.308,5.586-2.524,13.577-6.588C251.254,463.709,206.371,438.825,160.959,401.243z"></path> </g></svg>  
@@ -7,12 +18,12 @@
   </div>
 </section>
 
-
-
+<!-- Grid section with learning journal. Here are all the things I did in those sprints -->
 <section class="learning-journal" id="learning-journal">
   <h2>Learning Journal</h2>
 
 <div class="block-grid">
+
 <section class="sprint-13-block">
   <h3>Sprint 13</h3>
 </section>
@@ -46,12 +57,17 @@
 </section>
 </div>
 
+<nav class="navigation-bar">
+  <li><a href="#home">Heart</a></li>
+  <li><a href="#learning-journal">Learning Journal</a></li>
+  <li><a href="#we-love-web">We Love Web</a></li>
+  <li><a href="#digital-garden">Digital Garden</a></li>
+</nav>
+
 </section>
 
 <style>
-
-  .learning-journal{
-    height: 100vh;
+ .learning-journal{
 
     & h2{
       display: flex;
@@ -62,23 +78,22 @@
   }
   .block-grid{
     display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
     place-items: center;
-    grid-template-columns: 1fr 1fr;
-    gap: 1em;
-    padding: 10px;
-
-    & section{
-      height: 175px;
-      width: 175px;
+  }
+  .block-grid section{
       border-radius: 10px;
-      transition: 0.2s;
-
+      transition: 0.25s;
+      margin-bottom: 40px;
+      height: 150px;
+      width: 150px;
+      
       &:hover{
         transform: scale(1.1);
         cursor: pointer;
-      }
-    }
   }
+}
 
 .block-grid section:nth-of-type(1){
   background-color: red;
@@ -112,5 +127,33 @@ section:nth-of-type(8){
   background-color: coral;
 }
 
+@media only screen and (min-width: 623px) {
+    .block-grid{
+      grid-template-columns: repeat(3, 1fr);
+    }
+    .block-grid section{
+      height: 200px;
+      width: 200px;
+    }
+  }
 
+@media only screen and (min-width: 1024px) {
+    .block-grid{
+      grid-template-columns: repeat(4, 1fr);
+    }
+    .block-grid section{
+      height: 225px;
+      width: 225px;
+    }
+  }
+
+  @media only screen and (min-width: 1400px) {
+    .block-grid{
+      grid-template-columns: repeat(4, auto);
+    }
+    .block-grid section{
+      height: 300px;
+      width: 300px;
+    }
+  }
 </style>
